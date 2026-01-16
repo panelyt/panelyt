3aeecfddee62640dd206ab8cf490f0600a937ee6
egornomic <git@egornomic.xyz>
2026-01-16 17:22:51 +0000

    fix: annotate synthetic package mapping

--- apps/api/src/panelyt_api/optimization/service.py
+++ apps/api/src/panelyt_api/optimization/service.py
@@ -395,7 +395,7 @@ class OptimizationService:
 
         rows = (await self.session.execute(statement)).all()
         for item, offer, hist_min in rows:
-            mapping = None
+            mapping: SyntheticPackage | None = None
             if item.external_id in mapping_by_external:
                 mapping = mapping_by_external[item.external_id]
             elif item.slug in mapping_by_slug:
